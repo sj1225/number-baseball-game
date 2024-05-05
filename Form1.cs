17a92a9c1aec13ea49d5f6215bbf6727edc2b491
m1ngmy0ng <49314605+sj1225@users.noreply.github.com>
2024-05-05 19:54:20 +0900

    타이틀을 기본값에서 "숫자 야구 게임"으로 변경

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace number_baseball_game
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
